314bd326af242be6633bc563bc3789e6e6363d55
Vinicius Mazza <viniciusmazza2552@gmail.com>
2020-09-19 17:40:20 -0300

    .

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,8 @@
 // const { db } = require('./.env')
-// const DATABASE_URL = 'postgres://aoesuyzzftyoqs:9c82b02549ffd158f2c6a93f60cd29af5ff77b459be1431f805cce9e3044c60c@ec2-54-165-164-38.compute-1.amazonaws.com:5432/d4mgfm6i3m71th'
 module.exports = {
 
   client: 'postgresql',
-  // connection: DATABASE_URL,
+  connection: process.env.DATABASE_URL,
   pool: {
     min: 2,
     max: 10
