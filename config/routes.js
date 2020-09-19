2a7c5d5dfddef8fb1ee5e1b987558e9e138082fb
Vinicius Mazza <viniciusmazza2552@gmail.com>
2020-09-19 18:05:20 -0300

    .

--- config/routes.js
+++ config/routes.js
@@ -6,47 +6,47 @@ module.exports = app => {
   app.post('/validateToken', app.api.auth.validateToken)
 
   app.route('/users')
-    .all(app.config.passport.authenticate())
+    // .all(app.config.passport.authenticate())
     .post(app.api.user.save)
     .get(app.api.user.get)
 
   app.route('/users/:id')
-    .all(app.config.passport.authenticate())
+    // .all(app.config.passport.authenticate())
     .put(app.api.user.save)
     .delete(app.api.user.remove)
 
   app.route('/categories')
-    .all(app.config.passport.authenticate())
+    // .all(app.config.passport.authenticate())
     .get(admin(app.api.category.get))
     .post(app.api.category.save)
 
     // Tomar cuidado com a ordem! Tem que vir antes de /categories/:id
   app.route('/categories/tree')
-    .all(app.config.passport.authenticate())
+    // .all(app.config.passport.authenticate())
     .get(app.api.category.getTree)
 
   app.route('/categories/:id')
-    .all(app.config.passport.authenticate())
+    // .all(app.config.passport.authenticate())
     .get(app.api.category.getById)
     .put(app.api.category.save)
     .delete(app.api.category.remove)
   
   app.route('/clients')
-    .all(app.config.passport.authenticate())
+    // .all(app.config.passport.authenticate())
     .get(app.api.client.get)
     .post(app.api.client.save)
 
   app.route('/clients/:id')
-    .all(app.config.passport.authenticate())
+    // .all(app.config.passport.authenticate())
     .get(app.api.client.getById)
     .put(app.api.client.save)
     .delete(app.api.client.remove)
 
   app.route('/categories/:id/clients')
-    .all(app.config.passport.authenticate())
+    // .all(app.config.passport.authenticate())
     .get(app.api.client.getByCategory)
 
   app.route('/stats')
-    .all(app.config.passport.authenticate())
+    // .all(app.config.passport.authenticate())
     .get(app.api.stat.get)
 }
