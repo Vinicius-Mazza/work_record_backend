5deef8bab847df616bbef4d876e538651a2ffa96
Vinicius Mazza <viniciusmazza2552@gmail.com>
2020-10-04 18:53:43 -0300

    Nova tentativa de fazer o postgresql funcionar no heroku!

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,15 @@
-const { db } = require('./.env')
+// const { db } = require('./.env')
 
 module.exports = {
 
   client: 'postgresql',
-  connection: db,
+  connection: {
+    host : '127.0.0.1',
+    port: 5432,
+    database: 'work_record',
+    user: 'postgres',
+    password: '3301'
+  },
   pool: {
     min: 2,
     max: 10
@@ -11,6 +17,5 @@ module.exports = {
   migrations: {
     tableName: 'knex_migrations'
   }
-  
 
 };
