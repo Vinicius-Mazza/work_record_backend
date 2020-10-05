01d8d2d4d45f31bfab254cea69e387377a2c5a21
Vinicius Mazza <viniciusmazza2552@gmail.com>
2020-10-05 10:55:09 -0300

    Mais um teste

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 const { mongoDB } = require('../.env')
-const mongodb = mongodb ? process.env.mongodb : mongoDB
+const url = process.env.mongodb ? process.env.mongodb : mongoDB
 
-mongoose.connect(mongodb, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .catch(e => {
     const msg = 'ERRO! Não foi possível conectar com o MongoDB!'
     console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m')
